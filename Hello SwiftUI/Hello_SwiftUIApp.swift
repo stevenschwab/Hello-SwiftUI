c81053088e09fefc324e0f82b20f9b106fa77504
Steven Schwab <schwabs@umich.edu>
2023-01-23 13:43:47 -0600

    Initial Commit

---
+++ Hello SwiftUI/Hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftUIApp.swift
+//  Hello SwiftUI
+//
+//  Created by Steven Schwab on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
