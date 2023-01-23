ac26eb55095c6793ca54410a8747fcf8d791e288
Steven Schwab <schwabs@umich.edu>
2023-01-23 14:00:40 -0600

    finish

--- Hello SwiftUI/ContentView.swift
+++ Hello SwiftUI/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                TextField("Enter your name here", text: $name)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .border(/*@START_MENU_TOKEN@*/Color.pink/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Text(name.isEmpty ? "Hello!" : "Hello, \(name)!")
+                    .font(.title)
+                    .foregroundColor(Color.pink)
+                
+            }
+            .padding()
+            Button("Reset") {
+                name = ""
+            }
         }
-        .padding()
     }
 }
 
